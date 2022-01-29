d99568808e63f690fb792dd39b5b50e4306c3ba7
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-01-29 19:22:14 +0530

    lazy loaded components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
-import Cover from "./components/cover.js";
-import LoremSection from "./components/loremSection.js";
+import React, { lazy, Suspense } from "react";
+import Loader from "./components/Loader.jsx";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
+const Cover = lazy(() => import("./components/cover"));
+const LoremSection = lazy(() => import("./components/loremSection"));
 
 function App() {
   return (
     <Router basename={"/"}>
       <Switch>
         <Route path='/' exact={true}>
-          <Cover />
+          <Suspense fallback={<Loader />}>
+            <Cover />
+          </Suspense>
         </Route>
         <Router path='/lorem' exact={true}>
-          <LoremSection />
+          <Suspense fallback={<Loader />}>
+            <LoremSection />
+          </Suspense>
         </Router>
       </Switch>
     </Router>
